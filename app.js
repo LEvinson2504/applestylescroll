4229f98c141cf0cb22dd772e688da2dafa11e043
Levinson D'Souza <32845787+LEvinson2504@users.noreply.github.com>
2019-11-04 21:36:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const controller = new ScrollMagic.Controller();
 
 //Scenes
 const scene = new ScrollMagic.Scene({
-    duration: 4500,
+    duration: 9000,
     triggerElement: intro,
     triggerHook: 0
 })
@@ -24,7 +24,7 @@ let scrollpos = 0;
 let delay = 0;
 
 scene.on('update', e =>{
-    scrollpos = e.scrollPos/2000;
+    scrollpos = e.scrollPos/1000;
     //console.log(e);
 })
 
